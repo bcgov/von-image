96b5d809d90bfbf5b45ac1818bb71e0623bbb010
Andrew Whitehead <andrew@1crm.com>
2018-05-03 11:20:55 -0700

    add basic tests on docker image

--- make_image.py
+++ make_image.py
@@ -34,6 +34,7 @@ parser.add_argument('-n', '--name', default=DEFAULT_NAME, help='the base name fo
 parser.add_argument('-t', '--tag', help='a custom tag for the docker image')
 parser.add_argument('-f', '--file', help='use a custom Dockerfile')
 parser.add_argument('--build-arg', metavar='ARG=VAL', action='append', help='add docker build arguments')
+parser.add_argument('--dry-run', action='store_true', help='print docker command line instead of executing')
 parser.add_argument('--no-cache', action='store_true', help='ignore docker image cache')
 parser.add_argument('--py35', dest='python', action='store_const', const=PY_35_VERSION, help='use the default python 3.5 version')
 parser.add_argument('--py36', dest='python', action='store_const', const=PY_36_VERSION, help='use the default python 3.6 version')
@@ -42,12 +43,12 @@ parser.add_argument('--push', action='store_true', help='push the resulting imag
 parser.add_argument('-q', '--quiet', action='store_true', help='suppress output from docker build')
 parser.add_argument('--release', action='store_true', help='produce a release build of libindy')
 parser.add_argument('--squash', action='store_true', help='produce a smaller image')
-parser.add_argument('--test', action='store_true', help='print docker command line instead of executing')
+parser.add_argument('--test', action='store_true', help='perform tests on docker image')
 parser.add_argument('version', choices=VERSIONS.keys(), help='the predefined release version')
 
 args = parser.parse_args()
 py_ver = args.python or PY_35_VERSION
-reqs_path = 'requirements-' + args.version + '.txt'
+reqs_path = 'docker/requirements-' + args.version + '.txt'
 if not os.path.exists(reqs_path):
     raise Exception('Missing requirements file: {}'.format(reqs_path))
 
@@ -70,12 +71,16 @@ if not tag:
     if not args.release:
         tag += '-debug'
 
+dockerfile = 'docker/Dockerfile.ubuntu'
+if args.file:
+    dockerfile = args.file
+
 cmd_args = []
 for k,v in build_args.items():
     cmd_args.extend(['--build-arg', '{}={}'.format(k,v)])
 target = '.'
-if args.file:
-    cmd_args.extend(['-f', args.file])
+if dockerfile:
+    cmd_args.extend(['-f', dockerfile])
 if args.no_cache:
     cmd_args.append('--no-cache')
 if args.squash:
@@ -83,7 +88,7 @@ if args.squash:
 cmd_args.extend(['-t', tag])
 cmd_args.append(target)
 cmd = ['docker', 'build'] + cmd_args
-if args.test:
+if args.dry_run:
     print(' '.join(cmd))
 else:
     print('Building docker image...')
@@ -97,6 +102,25 @@ else:
         proc_sz = subprocess.run(['docker', 'image', 'inspect', tag, '--format={{.Size}}'], stdout=subprocess.PIPE)
         size = int(proc_sz.stdout.decode('ascii').strip()) / 1024.0 / 1024.0
         print('%0.2f%s' % (size, 'MB'))
+        if args.test or args.push:
+            test_path = 'Dockerfile.test'
+            with open(test_path, 'w') as docktest:
+                docktest.write('FROM {}\n'.format(tag))
+                docktest.write('COPY --chown=indy:indy test test\n')
+                docktest.write('USER root\n')
+                docktest.write('RUN test/setup.sh\n')
+                docktest.write('USER indy\n')
+                docktest.write('CMD test/validate.sh\n')
+            test_tag = tag + '-test'
+            proc_bt = subprocess.run(['docker', 'build', '-t', test_tag, '-f', test_path, '.'])
+            if proc_bt.returncode:
+                print('test image build failed')
+                sys.exit(1)
+            proc_test = subprocess.run(['docker', 'run', '--rm', '-ti', test_tag])
+            if proc_test.returncode:
+                print('One or more tests failed')
+                sys.exit(1)
+            print('All tests passed')
         if args.push:
             print('Pushing docker image...')
             proc = subprocess.run(['docker', 'push', tag])
