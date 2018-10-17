105b026aaced2d93cb5d1fc5be9fb1f42291d7ca
Andrew Whitehead <andrew@1crm.com>
2018-10-17 12:06:49 -0700

    updates for 1.6-8 image

--- make_image.py
+++ make_image.py
@@ -38,16 +38,16 @@ VERSIONS = {
         }
     },
     "1.6": {
-        "version": "1.6-7",
+        "version": "1.6-8",
         "args": {
-            # 1.6.5
-            "indy_sdk_url": "https://codeload.github.com/hyperledger/indy-sdk/tar.gz/84377c712550ca14db9365335054583c8ba7d435",
-            # 0.4.2
-            "indy_crypto_url": "https://codeload.github.com/hyperledger/indy-crypto/tar.gz/d04d8f4b0efbc6c86c203fd2b0d88ed9363ecc62",
+            # 1.6.7
+            "indy_sdk_url": "https://codeload.github.com/hyperledger/indy-sdk/tar.gz/0bb44c624fe4b64dd29d29200ebf6b208f6a9366",
+            # 0.4.5
+            "indy_crypto_url": "https://codeload.github.com/hyperledger/indy-crypto/tar.gz/a2864642430064c6f00902e9b999cc6356eed9f1",
         }
     },
     "1.6-ew": {
-        "version": "1.6-ew-7",
+        "version": "1.6-ew-8",
         "args": {
             # bcgov postgres_storage branch
             "indy_sdk_url": "https://codeload.github.com/bcgov/indy-sdk/tar.gz/04985a84d07f9e559266e2ba3bbdf3d0800ad570",
