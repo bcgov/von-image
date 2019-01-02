9a9e30e76e359d2343e87f9abac79df30d661f88
Ian Costanzo <ian@anon-solutions.ca>
2019-01-02 14:30:15 -0800

    Update indy-sdk commit number

--- make_image.py
+++ make_image.py
@@ -51,7 +51,7 @@ VERSIONS = {
         "version": "1.6-ew-14",
         "args": {
             # bcgov postgres_plugin branch
-            "indy_sdk_url": "https://codeload.github.com/ianco/indy-sdk/tar.gz/53acbbe4df29e7f114d0e34208663f97c4dd0ee1",
+            "indy_sdk_url": "https://codeload.github.com/ianco/indy-sdk/tar.gz/4359fe6886fc4c6ff95c8bad747cc7d4f0c1f865",
             # 0.5.0
             "indy_crypto_url": "https://codeload.github.com/hyperledger/indy-crypto/tar.gz/c323bd0046e4e7da936ad1682a401c557c74345b",
         }
